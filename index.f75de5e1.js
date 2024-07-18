70c4f06941281e24a7ee92110f082984128b78fa
Heorhii Savostikov <georgesavostikov@gmail.com>
2024-07-18 11:01:01 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,28 @@
+"use strict";
+function waitFor(element, eventName) {
+    const waitForPromise = new Promise((resolve, reject)=>{
+        element.addEventListener(eventName, ()=>{
+            resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
+        });
+    });
+    return waitForPromise;
+}
+const printMessage = (message)=>{
+    const divElement = document.createElement("div");
+    divElement.className = "message";
+    divElement.textContent = message;
+    document.body.append(divElement);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
